d2ab3053e454ab8c0633f2d7084882a093389386
Aurnytoraink <mathieu.heurtevinruiz@protonmail.com>
2023-02-14 17:35:06 +0100

    Détection disponibilté colonne

--- engine.c
+++ engine.c
@@ -5,6 +5,7 @@ unsigned char grille[7][6];
 unsigned char winner = 0;
 unsigned char current_player = 1;
 
+// Réinitialise la grille
 void reset_grille() {
     unsigned char x,y;
     for (x=0;x<7;x++) {
@@ -14,8 +15,19 @@ void reset_grille() {
     }
 }
 
-unsigned char is_avaible() {
-
+//Renvoie la ligne à laquelle le pion peut tomber
+//Renvoie -1 si la ligne est pleine
+signed char last_pos_avaible(unsigned char colonne) {
+    int i = 0;
+    if (grille[colonne][i] != 0) {
+        return -1;
+    }
+    for (i=1;i<6;i++) {
+        if (grille[colonne][i] != 0) {
+            break;
+        }
+    }
+    return i;
 }
 
 
