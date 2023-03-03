cc6e0cd8d56db5084c6fe60305e77880f6bb975a
Aurnytoraink <mathieu.heurtevinruiz@protonmail.com>
2023-03-03 11:19:55 +0100

    [WIP] Nouvelle base

--- engine.c
+++ engine.c
@@ -3,8 +3,9 @@
 
 // Var globale
 int grille[7][6];
-int winner = -1 ;
+int winner = -1;
 int current_player = 0;
+int filled_case = 0;
 
 // Réinitialise la grille
 void reset_grille()
@@ -38,28 +39,106 @@ int last_pos_avaible(int colonne)
     return i - 1;
 }
 
-// Renvoie 1 (vrai) si une ligne de 4 pions est détectée
-int is_winning_line(int x, int y)
+int search_pieces_in_line(int x_min, int x_max, int y_min, int y_max, int inverse)
 {
-    int i, j, vec_i, vec_j;
-    int pieces = 1;
-    int centre;
-    for (j = -1; j < 2; j++)
+    /*
+    Une boucle for à deux variables qui parcours tous les éléments
+    Il faut que le code return vrai dès que 4 pieces sont détectés
+    */
+    int x, y;
+    int pieces = 0;
+    if (inverse == 0)
     {
-        for (i = -1; i < 2; i++)
+        for (x = x_min, y = y_min; x < x_max && y < y_max; x++, y++)
         {
-            vec_i = i;
-            vec_j = j;
-            centre = (i==0 && j==0); //renvoie 1 si c'est la case pivot
-            while (!centre && x+vec_i >= 0 && x+vec_i <= 6 && y+vec_j >= 0 && y+vec_j <= 5 && grille[x+vec_i][y+vec_j] == current_player)
+            if (grille[x][y] == current_player)
             {
                 pieces++;
-                vec_i += i;
-                vec_j += j;
+            }
+            else
+            {
+                pieces = 0;
+            }
+
+            if (pieces == 4)
+            {
+                return 1;
             }
         }
     }
-    return (pieces >= 4);
+    else
+    {
+        for (x = x_min, y = y_min; x < x_max && y < y_max; x++, y--)
+        {
+            if (grille[x][y] == current_player)
+            {
+                pieces++;
+            }
+            else
+            {
+                pieces = 0;
+            }
+
+            if (pieces == 4)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+// Renvoie 1 (vrai) si une ligne de 4 pions est détectée
+int is_winning_line(int x, int y)
+{
+    int x_min, x_max, y_min, y_max;
+    int inverse = 0;
+    // VEC1 [1,1] diag sup
+    x_min = (x - 3 < 0) ? 0 : x - 3;
+    x_min = (x + 3 > 6) ? 6 : x + 3;
+    y_min = (y - 3 < 0) ? 0 : y - 3;
+    y_max = (y + 3 > 5) ? 5 : y + 3;
+
+    if (search_pieces_in_line(x_min, x_max, y_min, y_max, inverse) == 1)
+    {
+        return 1;
+    }
+
+    // VEC2 [0,1] ord
+    x_min = x;
+    x_max = x;
+    y_min = (y - 3 < 0) ? 0 : y - 3;
+    y_max = (y + 3 > 5) ? 5 : y + 3;
+
+    if (search_pieces_in_line(x_min, x_max, y_min, y_max, inverse) == 1)
+    {
+        return 1;
+    }
+
+    // VEC3 [1,0] abs
+    x_min = (x - 3 < 0) ? 0 : x - 3;
+    x_min = (x + 3 > 6) ? 6 : x + 3;
+    y_min = y;
+    y_max = y;
+
+    if (search_pieces_in_line(x_min, x_max, y_min, y_max, inverse) == 1)
+    {
+        return 1;
+    }
+
+    // VEC4 [1,-1] diag inf
+    inverse = 1;
+    x_min = (x - 3 < 0) ? 0 : x - 3;
+    x_min = (x + 3 > 6) ? 6 : x + 3;
+    y_min = (y + 3 > 5) ? 5 : y + 3;
+    y_max = (y - 3 < 0) ? 0 : y - 3;
+
+    if (search_pieces_in_line(x_min, x_max, y_min, y_max, inverse) == 1)
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 void Demarre_puissance4()
@@ -68,14 +147,16 @@ void Demarre_puissance4()
     int selected_column;
     int last_ligne;
 
-    while (winner == -1)
-    {
+    while (winner == -1 && filled_case != 42)
+    {   
+        printf("%d",filled_case);
         display_grille(grille);
         selected_column = ask_user_column(current_player);
         last_ligne = last_pos_avaible(selected_column);
         if (last_ligne != -1)
         {
             grille[selected_column][last_ligne] = current_player;
+            filled_case++;
             if (is_winning_line(selected_column, last_ligne) == 1)
             {
                 winner = current_player;
@@ -93,5 +174,13 @@ void Demarre_puissance4()
     }
 
     display_grille(grille);
-    display_winner(current_player);
+
+    if (filled_case == 42)
+    {
+        printf("Plus de place !\nVous êtes à égalité !");
+    }
+    else
+    {
+        display_winner(current_player);
+    }
 }
--- engine.h
+++ engine.h
@@ -1,4 +1,5 @@
 void Demarre_puissance4();
 void reset_grille();
 int last_pos_avaible(int colonne);
+int search_pieces_in_line(int x_min, int x_max, int y_min, int y_max, int inverse);
 int is_winning_line(int x, int y);
