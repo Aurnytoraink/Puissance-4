c0dbf7d94c3a178c8e64f76865ab583d035e3fe1
Mathieu Heurtevin <mathieu.heurtevinruiz@protonmail.com>
2023-03-12 19:55:40 +0100

    [WIP] Initiation à SDL

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-#include "engine.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void main() {
-    Demarre_puissance4();
-    // test_grille();
+int main(int argc, char *argv[])
+{
+    SDL_Window *window = NULL;
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              640, 480, SDL_WINDOW_SHOWN);
+    if(NULL == window)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    /* On agit sur la fenêtre ici */
+    SDL_Delay(3000);
+    SDL_Quit();
+return EXIT_SUCCESS;
 }
